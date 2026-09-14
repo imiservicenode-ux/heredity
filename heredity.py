6fd4178327354a6c1a74e17a8541a50f5c64cf67
imiservicenode-ux <imiservice.node@gmail.com>
2026-09-14 19:44:00 +0000

    Mon, Sep 14, 2026, 7:44 PM +00:00

--- heredity.py
+++ heredity.py
@@ -188,6 +188,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
          gene_probability = (mother_pass *(1- father_pass)) + ((1 - mother_pass) * father_pass)
     else:
         gene_probability = (1- mother_pass) * (1- father_pass)
+        
         trait_probability = PROBS["trait"][genes][trait]
 
     probability *= gene_probability * trait_probability
