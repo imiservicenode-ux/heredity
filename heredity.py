70976494aef4b4d7cec351b5aaad194f44b3b012
imiservicenode-ux <imiservice.node@gmail.com>
2026-09-14 16:34:43 +0000

    Mon, Sep 14, 2026, 4:34 PM +00:00

--- heredity.py
+++ heredity.py
@@ -274,9 +274,8 @@ def normalize(probabilities):
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
     for person in probabilities:
+
        gene_total = sum(probabilities[person]["genes"].values())
-
-
        for g in probabilities[person]["genes"][gene]:
            probabilities[person]["genes"][gene] /= gene_total
 
