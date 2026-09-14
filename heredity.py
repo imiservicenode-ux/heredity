b7826f6558aa82186f2ee875f9aceca5be04aed1
imiservicenode-ux <imiservice.node@gmail.com>
2026-09-14 19:39:20 +0000

    Mon, Sep 14, 2026, 7:39 PM +00:00

--- heredity.py
+++ heredity.py
@@ -183,7 +183,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
 
 
     if genes == 2:
-       gene_probability =(mother_pass*father_pass)
+       gene_probability = mother_pass*father_pass
     elif genes ==1:
          gene_probability = (mother_pass *(1- father_pass)+ (1 - mother_pass ) * father_pass)
     else:
