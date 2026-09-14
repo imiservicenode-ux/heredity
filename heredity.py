6385be493aa243c9582286d7623bde2ae9f43b8f
imiservicenode-ux <imiservice.node@gmail.com>
2026-09-14 17:14:45 +0000

    Mon, Sep 14, 2026, 5:14 PM +00:00

--- heredity.py
+++ heredity.py
@@ -241,8 +241,8 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
         else:
              trait = False
 
-        probabilities [person]["genes"][genes] += p
-        probabilities [person]["trait"][trait] += p
+        probabilities[person]["genes"][genes] += p
+        probabilities[person]["trait"][trait] += p
 
 
 
