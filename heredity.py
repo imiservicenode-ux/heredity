89c8a4aad79bef5871ddadeaadccd18ebf6093b1
imiservicenode-ux <imiservice.node@gmail.com>
2026-09-14 16:32:12 +0000

    Mon, Sep 14, 2026, 4:32 PM +00:00

--- heredity.py
+++ heredity.py
@@ -261,7 +261,7 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
              #joint probabilities = P distribution
 
 
-             probabilities [person]["gene"][genes] += p
+             probabilities [person]["genes"][genes] += p
              probabilities [person]["trait"][trait]+= p
 
 
